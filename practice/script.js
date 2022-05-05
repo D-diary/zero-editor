4047b6b454027b2655f5ceb81a82b5e70643a9aa
D-diary <dlrlsud@naver.com>
2022-05-05 13:54:59 +0900

    중간저장

--- practice/script.js
+++ practice/script.js
@@ -1,11 +1,95 @@
 ;(function () {
   'use strict'
 
+  const commands = [
+    {
+      cmd: 'backColor',
+      val: 'red',
+      label: '배경 컬러',
+    },
+    {
+      cmd: 'bold',
+      label: '굵기',
+    },
+    {
+      cmd: 'delete',
+      label: '삭제',
+    },
+    {
+      cmd: 'fontSize',
+      label: '폰트 사이즈',
+      val: '1-7',
+    },
+    {
+      cmd: 'foreColor',
+      label: '폰트 컬러',
+      val: 'rgba(0,0,0,.5)',
+    },
+    {
+      cmd: 'insertImage',
+      label: '이미지 추가',
+      val: 'http://dummyimage.com/160x90',
+    },
+    {
+      cmd: 'italic',
+      label: '기울이기',
+    },
+    {
+      cmd: 'justifyCenter',
+      label: '중앙 정렬',
+    },
+    {
+      cmd: 'justifyFull',
+      label: '양쪽 정렬',
+    },
+    {
+      cmd: 'justifyLeft',
+      label: '좌측 정렬',
+    },
+    {
+      cmd: 'justifyRight',
+      label: '우측 정렬',
+    },
+
+    {
+      cmd: 'selectAll',
+      label: '전체 선택',
+    },
+    {
+      cmd: 'underline',
+      label: '밑줄',
+    },
+    {
+      cmd: 'undo',
+      label: '취소',
+    },
+  ]
+
+  const commandObject = {}
+
   const get = (target) => {
     return document.querySelector(target)
   }
 
-  const init = () => {}
+  const doCommand = (cmdKey) => {
+    const cmd = commandObject[cmdKey]
+  }
+  
+  const $editorButtons = get('.editor_buttons')
+
+  const init = () => {
+    commands.map((command) => {
+      commandObject[command.cmd] = command
+      console.log(commandObject)
+      const element = document.createElement('button')
+      element.innerText = command.label
+      element.addEventListener('click', (e) => {
+        e.preventDefault()
+        doCommand(command.cmd)
+      })
+      $editorButtons.appendChild(element)
+    })
+  }
 
   init()
 })()
